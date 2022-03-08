d43b7c486ed4ee863f11fc409bacf019136f4af5
owenCosinga <owenpsicosis@gmail.com>
2022-03-08 17:13:57 -0500

    este es una clase de como separar la logica y el diseño, de como pasar codigo secundario a la clase principal

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_curso/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Center(child: Text('Hola Mundo')),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
